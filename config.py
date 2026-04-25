83e90fada84ba500feb818c609c8363ae52bfcc7
subhs83 <subhash.ece@gmail.com>
2026-04-25 17:24:41 +0530

    Add psycopg2 for PostgreSQL support

--- config.py
+++ config.py
@@ -2,14 +2,18 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-key")
-   # SECRET_KEY = "super-secret-key"
-    
+
     BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
-    # SQLite database
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(BASE_DIR, "exam.db")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
+    DATABASE_URL = os.getenv("DATABASE_URL")
 
-    # Optional: directory for uploads, logs, etc.
+    # Fix for Render (VERY IMPORTANT)
+    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+    # ✅ Use Postgres if available, else fallback to SQLite (for local)
+    SQLALCHEMY_DATABASE_URI = DATABASE_URL or "sqlite:///" + os.path.join(BASE_DIR, "exam.db")
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
     UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
