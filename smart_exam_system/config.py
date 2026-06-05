ea142f82b2fac8f937543e0cdc2442541b2cbc4c
Subhash Chaurasia <subhash.ece@gmail.com>
2026-06-05 16:41:38 +0530

    Stable Flask version before React migration

--- smart_exam_system/config.py
+++ smart_exam_system/config.py
@@ -7,13 +7,13 @@ class Config:
 
     # 🔥 FORCE DATABASE URL CLEANLY
     database_url = os.environ.get("DATABASE_URL")
-
+    
     # Fix Render postgres issue
     if database_url and database_url.startswith("postgres://"):
         database_url = database_url.replace("postgres://", "postgresql://", 1)
 
     SQLALCHEMY_DATABASE_URI = database_url or "sqlite:///" + os.path.join(BASE_DIR, "exam.db")
-
+    print("DATABASE:", SQLALCHEMY_DATABASE_URI)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
